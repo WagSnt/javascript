e9c0bf54dc790b3a8fe9768152ab095f420eb6b3
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2022-11-29 07:48:55 -0300

    Update matrizes.js

--- onebitcode/modulo1/aula08/matrizes.js
+++ onebitcode/modulo1/aula08/matrizes.js
@@ -4,8 +4,7 @@ const arr = [
   [
     ['3º Nível, 1º Item', 'Olá, Mundo!'],
     ['3º Nível, 2º Item', 'Oi, Mundo!'],
-  ],
-  []
+  ]
 ]
 
 console.log(arr)
@@ -15,3 +14,18 @@ console.log(arr[1][0])
 console.log(arr[1][1])
 console.log(arr[2][1])
 console.log(arr[2][1][0])
+
+const matriz = [
+  ['l1, c1', 'l1, c2', 'l1, c3', 'l1, c4'],
+  ['l2, c1', 'l2, c2', 'l2, c3', 'l2, c4'],
+  ['l3, c1', 'l3, c2', 'l3, c3', 'l3, c4'],
+]
+
+console.table(matriz)
+
+for (let i = 0; i < matriz.length; i++)
+for (let j = 0; j <matriz[i].length; j++)
+const element = matriz[i][j]
+console.log('posição: (' + i +  ', ' + j + ') valor: '  + element)
+
+console.log(matriz[1])
