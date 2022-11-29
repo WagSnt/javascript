94e69cdc2e229aebf04bf26bf78dda2275fbc284
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2022-11-29 16:29:03 -0300

    Exercício de funções

---
+++ onebitcode/modulo1/aula12/retorno.js
@@ -0,0 +1,19 @@
+function calcularMedia(a, b) {
+  const media = (a + b) / 2
+  return media
+}
+
+const resultado = calcularMedia(7, 2)
+console.log(resultado)
+
+function criarProduto(nome, preco) {
+  const produto = {
+  nome,
+  preco,
+  estoque: 1
+  }
+  return produto
+}
+
+const notebook = criarProduto('Acer Intel Core i3', 2500)
+console.log(notebook)
---
+++ onebitcode/modulo1/exercicios/calculadora_geometrica/geometrica.js
@@ -0,0 +1,52 @@
+let options = ''
+
+do {
+  options = prompt(
+    'CALCULADORA GEOMÉTRICA\n\n' +
+    'Escolha abaixo, qual cálculo deseja realizar:' + 
+    '\n1 - Área do triângulo' +
+    '\n2 - Área do retângulo' +
+    '\n3 - Área do quadrado' +
+    '\n4 - Área do trapézio' +
+    '\n5 - Área do triângulo' +
+    '\n6 - Sair'
+  )
+
+  switch (options) {
+    case '1':
+      function triangleArea(a, b) {
+        const result = (a * b) / 2
+        return parseFloat(result)
+      }
+
+      let base = parseFloat(prompt('Informe a base do triângulo:'))
+      const triangleHeight = parseFloat(prompt('Informe a altura do triângulo:'))
+      let finalResult = triangleArea(base, triangleHeight)
+
+      alert('Área do triângulo: ' + base + ' x ' + triangleHeight + ' = ' + finalResult)
+      break
+    case '2':
+      function rectangleArea (a, b) {
+        const result = a * b
+        return result
+      }
+
+      const rectangleBase = parseFloat(prompt('Informe a base do retângulo:'))
+      const rectangleHeight = parseFloat(prompt('Informe a altura do retângulo'))
+      let rectangleResult = rectangleArea(rectangleBase, rectangleHeight)
+      
+      alert('Área do Retângulo: ' + rectangleBase + ' x ' + rectangleHeight + ' = ' + rectangleResult)
+    case '3':
+      function squareArea(){
+      let side = prompt('Digite o valor do lado:')
+      result = side * side
+      return result
+    }
+    alert('Área do Quadrado: ' + side + '² = ' + result)
+
+    case '4':
+    case '5':
+    case '6':
+    default:
+  }
+} while(options !== '6')
