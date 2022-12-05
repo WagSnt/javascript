cbfd68d42f5f7022c636315936f63f756741a804
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2022-12-05 09:28:53 -0300

    Exercício de DOM

--- onebitcode/modulo_dom/exercicios/escalacao_time/index.js
+++ onebitcode/modulo_dom/exercicios/escalacao_time/index.js
@@ -1,55 +1,41 @@
-function addPlayer () {
-  const lineupSection = document.getElementById('lineup')
+function addPlayer() {
+  const position = document.getElementById('playerposition').value
+  const name = document.getElementById('playername').value
+  const number = document.getElementById('playernumber').value
 
-  const h3 = document.createElement('h3')
-  h3.type = 'text'
-  h3.innerText = 'Jogador'
+  const confirmation = confirm(
+    'Tem certeza que deseja escalar este jogador?\n' +
+    position + ' - ' + name + ': Nº ' + number
+    )
 
-  const ul = document.createElement('ul')
+  if (confirmation) {
+    const lineupSection = document.getElementById('lineup')
+  const player = document.createElement('li')
+  player.id = 'player-' + number
+  player.innerText = position + ' - ' + name + ': Nº ' + number
+  lineupSection.appendChild(player)
 
-  const positionLi = document.createElement('li')
-  positionLi.innerText = 'Posição: '
-  const positionInput = document.createElement('input')
-  positionInput.type = 'text'
-  positionInput.name = 'playerposition'
-  positionLi.appendChild(positionInput)
-  ul.appendChild(positionLi)
-
-  const nameLi = document.createElement('li')
-  nameLi.innerText = 'Nome do jogador: '
-  const nameInput = document.createElement('input')
-  nameInput.type = 'text'
-  nameInput.name = 'palyername'
-  nameLi.appendChild(nameInput)
-  ul.appendChild(nameLi)
-
-  const numberLi = document.createElement('li')
-  numberLi.innerText = 'Número da camisa: '
-  const numberInput = document.createElement('input')
-  numberInput.type = 'number'
-  numberInput.name = 'shirtnumber'
-  numberLi.append(numberInput)
-  ul.appendChild(numberLi)
-
-  lineupSection.append(h3, ul)
+  document.getElementById('playerposition').value = ''
+  document.getElementById('playername').value = ''
+  document.getElementById('playernumber').value = ''
+  } else {
+    alert('Escalação de jogador cancelada...')
+    document.getElementById('playerposition').value = ''
+    document.getElementById('playername').value = ''
+    document.getElementById('playernumber').value = ''
+  }
 }
 
 function removePlayer() {
-  const lineupSection = document.getElementById('lineup')
-  const h3 = document.createElement('h3')
-  h3.type = 'text'
-  h3.innerText = 'Remover jogador'
+  const number = document.getElementById('remove-number').value
+  const playerNumber = document.getElementById('player-' + number)
 
-  const ul = document.createElement('ul')
+  const confirmation = confirm('Tem certeza que deseja remover este jogador?\n' + playerNumber.innerText)
 
-  const numberLi = document.createElement('li')
-  numberLi.innerText = 'Digite o número do jogador que deseja remover: '
-  const numberInput = document.createElement('input')
-  numberInput.type = 'text'
-  numberInput.name = 'removeplayer'
-  const buttonRemove = document.createElement('button')
-  buttonRemove.innerText = 'Remover'
-  numberLi.append(numberInput, buttonRemove)
-  ul.appendChild(numberLi)
-  lineupSection.append(h3, ul)
+  if (confirmation) {
+    playerNumber.remove()
+  } else {
+    alert('Remoção cancelada...')
+    document.getElementById('remove-number').value = ''
+  }
 }
