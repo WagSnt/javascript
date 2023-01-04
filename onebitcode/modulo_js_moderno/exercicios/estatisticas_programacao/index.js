603d0ffed5308dd88c9d91af274eb751578df08f
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2023-01-04 08:09:32 -0300

    Atualização

---
+++ onebitcode/modulo_js_moderno/exercicios/estatisticas_programacao/index.js
@@ -0,0 +1,8 @@
+// Média Aritmética Simples
+const numbers = [5, 5]
+function simpleArithmetic(...numbers) {
+  let i = 0
+  const medianum = numbers.reduce((accum, num) => accum + num, 0)
+  media = medianum / 
+}
+
