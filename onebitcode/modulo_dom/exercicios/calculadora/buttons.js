1d0dc108c0001c1671bccfaabe12f9fe3561ef63
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2023-01-04 22:13:09 -0300

    update

---
+++ onebitcode/modulo_dom/exercicios/calculadora/buttons.js
@@ -0,0 +1,40 @@
+import {calculate} from './calculate.js'
+const input = document.getElementById('calculationArea')
+const resultInput = document.getElementById('result')
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "] //lista de caracteres permitidos para digitação
+
+//clear
+function clearBtn() {
+  input.value = ''
+  input.focus() // colocando o cursor novamente no input, pronto para digitação
+}
+
+//teclado pressionado
+function keydownBtn(ev) { //keydown (botão pressionado)
+  ev.preventDefault() //evitando que seja inserido o valor da tecla pressionada
+  if (allowedKeys.includes(ev.key)) {//a lista de permissão possui o valor da tecla pressionada pelo usuário?
+    input.value += ev.key //inserindo o valor no input
+    return
+  } 
+  if (ev.key === 'Backspace') { //configurando backspace
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === 'Enter') { //configurando enter para mostrar o resultado do calculo
+    calculate()
+  }
+}
+
+//Copy
+function copy(ev) {
+  const button = ev.currentTarget //pegando quem acionou o evento
+  if (button.innerText === 'Copy') { // pegando o texto 'copy', que indica que ainda não foi copiado
+    button.innerText = 'Copied!'
+    button.classList.add('success') // estilizando a borda e a fonte
+    navigator.clipboard.writeText(resultInput.value) //copiando o resultado (navigator é disponibilizado através do window.navigator)
+  } else {
+    button.innerText = 'Copy'
+    button.classList.remove('success')
+  }
+}
+
+export {clearBtn, keydownBtn, copy}
--- onebitcode/modulo_dom/exercicios/calculadora/calculadora.js
+++ onebitcode/modulo_dom/exercicios/calculadora/calculadora.js
@@ -1,12 +1,11 @@
+import {calculate} from './calculate.js'
+import {clearBtn, keydownBtn, copy} from './buttons.js'
+import {theme} from './theme.js'
+
 const main = document.querySelector('main')
 const root = document.querySelector(':root')
 const input = document.getElementById('calculationArea')
-const resultInput = document.getElementById('result')
-
-//Digitação na calculadora
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "] //lista de caracteres permitidos para digitação
-
-//configurando teclas
+//configurando botões
 document.querySelectorAll('.charKey').forEach(function(charKeyBtn) { //pegando a classe de botões
   charKeyBtn.addEventListener('click', function() { // dando função para cada botão
     const value = charKeyBtn.dataset.value // pegando o valor de cada botão
@@ -15,65 +14,16 @@ document.querySelectorAll('.charKey').forEach(function(charKeyBtn) { //pegando a
 })
 
 //configurando o clear
-document.getElementById('clear').addEventListener('click', function() {
-  input.value = ''
-  input.focus() // colocando o cursor novamente no input, pronto para digitação
-})
+document.getElementById('clear').addEventListener('click', clearBtn)
 
-input.addEventListener('keydown', function(ev) { //keydown (botão pressionado)
-  ev.preventDefault() //evitando que seja inserido o valor da tecla pressionada
-  if (allowedKeys.includes(ev.key)) {//a lista de permissão possui o valor da tecla pressionada pelo usuário?
-    input.value += ev.key //inserindo o valor no input
-    return
-  } 
-  if (ev.key === 'Backspace') { //configurando backspace
-    input.value = input.value.slice(0, -1)
-  }
-  if (ev.key === 'Enter') { //configurando enter para mostrar o resultado do calculo
-    calculate()
-  }
-})
+//configurando teclado
+input.addEventListener('keydown', keydownBtn)
 
-//configurando o botão de igual
+//configurando o botão de igual // configurando o calculo
 document.getElementById('equal').addEventListener('click', calculate)
 
-// configurando o calculo
-function calculate() {
-  resultInput.value = 'ERROR'
-  resultInput.classList.add('error')
-
-  const result = eval(input.value)
-  
-  resultInput.value = result
-  resultInput.classList.remove('error')
-}
-
 //copiando resultado para a área de transferências
-document.getElementById('copyButton').addEventListener('click', function(ev) {
-  const button = ev.currentTarget //pegando quem acionou o evento
-  if (button.innerText === 'Copy') { // pegando o texto 'copy', que indica que ainda não foi copiado
-    button.innerText = 'Copied!'
-    button.classList.add('success') // estilizando a borda e a fonte
-    navigator.clipboard.writeText(resultInput.value) //copiando o resultado (navigator é disponibilizado através do window.navigator)
-  } else {
-    button.innerText = 'Copy'
-    button.classList.remove('success')
-  }
-})
+document.getElementById('copyButton').addEventListener('click', copy)
 
 //trocando o tema da calculadora
-document.getElementById('themeSwitcher').addEventListener('click', function() {
-  if (main.dataset.theme === 'dark') { //verificando qual é o tema atual
-    root.style.setProperty('--bg-color', '#f1f5f9') //acessando as propriedades do :root
-    root.style.setProperty('--border-color', '#aaa')
-    root.style.setProperty('--font-color', '#212529')
-    root.style.setProperty('--primary-color', '#26834a')
-    main.dataset.theme = 'light'
-  } else {
-    root.style.setProperty('--bg-color', '#212529')
-    root.style.setProperty('--border-color', '#666')
-    root.style.setProperty('--font-color', '#f1f5f9')
-    root.style.setProperty('--primary-color', '#4dff91')
-    main.dataset.theme = 'dark'
-  }
-})
+document.getElementById('themeSwitcher').addEventListener('click', theme)
